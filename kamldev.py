a798010823a5659140dabfa610fceba512d46ed3
D.Kamal basha <dudekulakamal72@gmail.com>
2023-05-18 13:17:53 +0530

    first commit

---
+++ kamldev.py
@@ -0,0 +1,11 @@
+#https://github.com/kamal5572/Assignment-6.git
+if __name__ == '__main__':
+    #Arithmetic Operations
+    a = int(input())
+    b = int(input())
+    c = a + b
+    d = a - b
+    e = a * b
+    print(c,d,e,sep = "\n")
+
+    print(f"{a // b}\n{a / b}")
